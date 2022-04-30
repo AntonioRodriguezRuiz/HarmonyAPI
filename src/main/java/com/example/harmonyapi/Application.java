35e491d96a56f13c16a329641f105cd34cd020ea
AntonioRodriguezRuiz <antoniorodriguezruiz2002@gmail.com>
2022-04-30 17:25:53 +0200

    removed unused parameter

--- src/main/java/com/example/harmonyapi/Application.java
+++ src/main/java/com/example/harmonyapi/Application.java
@@ -4,9 +4,8 @@ import api.MediaController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(exclude = { R2dbcAutoConfiguration.class }, scanBasePackages = "api")
+@SpringBootApplication(scanBasePackages = "api")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);

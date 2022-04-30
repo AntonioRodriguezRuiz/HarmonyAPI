aa465c0853e3249b93e029bc23f25da5a63665ba
AntonioRodriguezRuiz <antoniorodriguezruiz2002@gmail.com>
2022-04-30 17:24:43 +0200

    Added getAllMedia()

---
+++ src/main/java/api/MediaController.java
@@ -0,0 +1,25 @@
+package api;
+
+import api.Services.MediaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import src.main.java.HarmonyDatabase.tables.pojos.Media;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/api/v1/media")
+public class MediaController {
+
+    @Autowired
+    MediaService mediaService;
+
+    @GetMapping
+    public List<Media> getAllMedia(){
+        return mediaService.getAllMedia();
+    }
+}

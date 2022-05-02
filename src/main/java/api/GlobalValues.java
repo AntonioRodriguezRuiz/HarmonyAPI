8b836d40f4c6b0a19422408bf0c77b8729a51a77
AntonioRodriguezRuiz <antoniorodriguezruiz2002@gmail.com>
2022-05-02 12:32:16 +0200

    Added GlobalValues

---
+++ src/main/java/api/GlobalValues.java
@@ -0,0 +1,8 @@
+package api;
+
+public class GlobalValues {
+
+    public static final String USER = "user";
+    public static final String PASSWORD = "user";
+    public static final String URL = "jdbc:mariadb://localhost:3306/harmony";
+}

daaab6efdefd8a9aa60398d8e4967041fd5d605f
AntonioRodriguezRuiz <antoniorodriguezruiz2002@gmail.com>
2022-05-02 11:59:39 +0200

    Added query parameters to the equation

--- src/main/java/api/Controllers/MediaController.java
+++ src/main/java/api/Controllers/MediaController.java
@@ -1,14 +1,24 @@
 package api.Controllers;
 
 import api.Services.MediaService;
+import org.jooq.OrderField;
+import org.jooq.SortField;
+import org.jooq.TableField;
+import org.jooq.TableLike;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.server.ResponseStatusException;
+import src.main.java.HarmonyDatabase.Tables;
 import src.main.java.HarmonyDatabase.tables.pojos.Media;
 
 import java.util.List;
+import java.util.Map;
+
+import static src.main.java.HarmonyDatabase.Tables.*;
 
 
 @RestController
@@ -19,7 +29,36 @@ public class MediaController {
     MediaService mediaService;
 
     @GetMapping
-    public List<Media> getAllMedia(){
-        return mediaService.getAllMedia();
+    public List<Media> getAllMedia(@RequestParam Map<String, String> allRequestParams) {
+        String search = allRequestParams.containsKey("search") ? allRequestParams.get("search") : "";
+
+        String typeString = allRequestParams.containsKey("type") ? allRequestParams.get("type") : null;
+        TableLike typeTable = null;
+
+        String orderString =  allRequestParams.containsKey("order") ? allRequestParams.get("order") : null;
+        SortField orderField = null;
+
+        if(typeString!=null){
+            switch (typeString){
+                case "movie": typeTable = Tables.MOVIES; break;
+                case "series": typeTable = Tables.SERIES; break;
+                case "videogame": typeTable = Tables.VIDEOGAMES; break;
+                case "book": typeTable = Tables.BOOKS; break;
+                default: throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+            }
+        }
+
+        if(orderString!=null){
+            switch (orderString){
+                case "title": orderField = MEDIA.TITLE.asc(); break;
+                case "releaseDate": orderField = MEDIA.RELEASEDATE.desc(); break;
+                case "rating": orderField = MEDIA.AVGRATING.desc(); break;
+                default: throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+            }
+        }
+
+        String order = allRequestParams.containsKey("order") ? allRequestParams.get("order") : null;
+
+        return mediaService.getAllMedia(search, typeTable, orderField);
     }
 }
--- src/main/java/api/Services/MediaService.java
+++ src/main/java/api/Services/MediaService.java
@@ -1,34 +1,60 @@
 package api.Services;
 
-import org.jooq.DSLContext;
-import org.jooq.SQLDialect;
+import api.BodyRequestHelpers.MediaRequest;
+import api.GlobalValues;
+import org.jooq.*;
 import org.jooq.impl.DSL;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 import src.main.java.HarmonyDatabase.tables.pojos.Media;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.List;
 
-import static src.main.java.HarmonyDatabase.Tables.MEDIA;
+import static src.main.java.HarmonyDatabase.Tables.*;
 
 @Service
 public class MediaService {
 
-    public List<Media> getAllMedia(){
-        String userName = "user";
-        String password = "user";
-        String url = "jdbc:mariadb://localhost:3306/harmony";
-        DSLContext create;
+    public List<Media> getAllMedia(String search, TableLike type, SortField order) {
         List<Media> result = null;
 
-        try (Connection conn = DriverManager.getConnection(url, userName, password)) {
-            create = DSL.using(conn, SQLDialect.MARIADB);
-            result = create.select()
-                    .from(MEDIA)
-                    .fetchInto(Media.class);
-        } catch (Exception e){
-            e.printStackTrace();
+        try (Connection conn = DriverManager.getConnection(GlobalValues.URL, GlobalValues.USER, GlobalValues.PASSWORD)) {
+            DSLContext create = DSL.using(conn, SQLDialect.MARIADB);
+            if (type==null && order==null){
+                result = create.select(MEDIA.fields())
+                        .from(MEDIA)
+                        .where(MEDIA.TITLE.contains(search))
+                        .fetchInto(Media.class);
+
+            } else if (order==null){
+                result = create.select(MEDIA.fields())
+                        .from(MEDIA)
+                        .naturalJoin(type)
+                        .where(MEDIA.TITLE.contains(search))
+                        .fetchInto(Media.class);
+
+            } else if (type==null){
+                result = create.select(MEDIA.fields())
+                        .from(MEDIA)
+                        .where(MEDIA.TITLE.contains(search))
+                        .orderBy(order)
+                        .fetchInto(Media.class);
+
+            } else{
+                result = create.select(MEDIA.fields())
+                        .from(MEDIA)
+                        .naturalJoin(type)
+                        .where(MEDIA.TITLE.contains(search))
+                        .orderBy(order)
+                        .fetchInto(Media.class);
+            }
+
+        } catch (Exception exception){
+            exception.printStackTrace();
         }
         return result;
     }

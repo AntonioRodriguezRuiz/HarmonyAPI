c1a2b5dff5e964d8e4b9d9aa492dd6a2d5cef877
AntonioRodriguezRuiz <antoniorodriguezruiz2002@gmail.com>
2022-05-02 11:58:34 +0200

    removed unused imports

--- src/main/java/com/example/harmonyapi/Application.java
+++ src/main/java/com/example/harmonyapi/Application.java
@@ -1,9 +1,7 @@
 package com.example.harmonyapi;
 
-import api.MediaController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration;
 
 @SpringBootApplication(scanBasePackages = "api")
 public class Application {

0749e1c0b9cd5b813dd4c28fb46afa808488be55
AntonioRodriguezRuiz <antoniorodriguezruiz2002@gmail.com>
2022-05-02 12:59:39 +0200

    Added genre filter

--- src/main/java/api/Controllers/MediaController.java
+++ src/main/java/api/Controllers/MediaController.java
@@ -1,10 +1,12 @@
 package api.Controllers;
 
+import api.GlobalValues;
 import api.Services.MediaService;
-import org.jooq.OrderField;
+import org.jooq.DSLContext;
+import org.jooq.SQLDialect;
 import org.jooq.SortField;
-import org.jooq.TableField;
 import org.jooq.TableLike;
+import org.jooq.impl.DSL;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,12 +15,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 import src.main.java.HarmonyDatabase.Tables;
+import src.main.java.HarmonyDatabase.tables.pojos.Genres;
 import src.main.java.HarmonyDatabase.tables.pojos.Media;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
 
-import static src.main.java.HarmonyDatabase.Tables.*;
+import static src.main.java.HarmonyDatabase.Tables.GENRES;
+import static src.main.java.HarmonyDatabase.Tables.MEDIA;
 
 
 @RestController
@@ -29,15 +36,16 @@ public class MediaController {
     MediaService mediaService;
 
     @GetMapping
-    public List<Media> getAllMedia(@RequestParam Map<String, String> allRequestParams) {
+    public List<Media> getAllMedia(@RequestParam Map<String, String> allRequestParams) throws SQLException {
         String search = allRequestParams.containsKey("search") ? allRequestParams.get("search") : "";
+
+        String genreString = allRequestParams.containsKey("genre") ? allRequestParams.get("genre"): null;
+        if(genreString!=null){
+            isValidGenre(genreString);
+        }
 
         String typeString = allRequestParams.containsKey("type") ? allRequestParams.get("type") : null;
         TableLike typeTable = null;
-
-        String orderString =  allRequestParams.containsKey("order") ? allRequestParams.get("order") : null;
-        SortField orderField = null;
-
         if(typeString!=null){
             switch (typeString){
                 case "movie": typeTable = Tables.MOVIES; break;
@@ -48,6 +56,8 @@ public class MediaController {
             }
         }
 
+        String orderString =  allRequestParams.containsKey("order") ? allRequestParams.get("order") : null;
+        SortField orderField = null;
         if(orderString!=null){
             switch (orderString){
                 case "title": orderField = MEDIA.TITLE.asc(); break;
@@ -57,8 +67,27 @@ public class MediaController {
             }
         }
 
-        String order = allRequestParams.containsKey("order") ? allRequestParams.get("order") : null;
+        return mediaService.getAllMedia(search, typeTable, orderField, genreString);
+    }
 
-        return mediaService.getAllMedia(search, typeTable, orderField);
+    private void isValidGenre(String genreString) throws SQLException {
+        try (Connection conn = DriverManager.getConnection(GlobalValues.URL, GlobalValues.USER, GlobalValues.PASSWORD)) {
+            DSLContext create = DSL.using(conn, SQLDialect.MARIADB);
+            List<Genres> result = create.select()
+                                        .from(GENRES)
+                                        .where(GENRES.NAME.eq(genreString))
+                                        .fetchInto(Genres.class);
+
+            Boolean res = result.stream().anyMatch(g -> g.getName().equals(genreString));
+            if(!res){
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+            }
+
+        } catch (ResponseStatusException | SQLException e){
+            if(e instanceof ResponseStatusException){
+                throw e;
+            }
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/api/Services/MediaService.java
+++ src/main/java/api/Services/MediaService.java
@@ -1,17 +1,16 @@
 package api.Services;
 
-import api.BodyRequestHelpers.MediaRequest;
 import api.GlobalValues;
-import org.jooq.*;
+import org.jooq.DSLContext;
+import org.jooq.SQLDialect;
+import org.jooq.SortField;
+import org.jooq.TableLike;
 import org.jooq.impl.DSL;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 import src.main.java.HarmonyDatabase.tables.pojos.Media;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.util.List;
 
 import static src.main.java.HarmonyDatabase.Tables.*;
@@ -19,36 +18,71 @@ import static src.main.java.HarmonyDatabase.Tables.*;
 @Service
 public class MediaService {
 
-    public List<Media> getAllMedia(String search, TableLike type, SortField order) {
+    public List<Media> getAllMedia(String search, TableLike type, SortField order, String genre) {
         List<Media> result = null;
 
         try (Connection conn = DriverManager.getConnection(GlobalValues.URL, GlobalValues.USER, GlobalValues.PASSWORD)) {
             DSLContext create = DSL.using(conn, SQLDialect.MARIADB);
-            if (type==null && order==null){
+            if (type==null && order==null && genre==null){
                 result = create.select(MEDIA.fields())
                         .from(MEDIA)
                         .where(MEDIA.TITLE.contains(search))
                         .fetchInto(Media.class);
 
-            } else if (order==null){
+            } else if (order==null && genre==null){
                 result = create.select(MEDIA.fields())
                         .from(MEDIA)
                         .naturalJoin(type)
                         .where(MEDIA.TITLE.contains(search))
                         .fetchInto(Media.class);
 
-            } else if (type==null){
+            } else if (type==null && order==null){
+                result = create.select(MEDIA.fields())
+                        .from(MEDIA)
+                        .naturalJoin(MEDIAGENRES)
+                        .naturalJoin(GENRES)
+                        .where(MEDIA.TITLE.contains(search).and(GENRES.NAME.eq(genre)))
+                        .fetchInto(Media.class);
+
+            } else if (type==null && genre==null){
                 result = create.select(MEDIA.fields())
                         .from(MEDIA)
                         .where(MEDIA.TITLE.contains(search))
                         .orderBy(order)
                         .fetchInto(Media.class);
 
-            } else{
+            } else if (genre==null){
                 result = create.select(MEDIA.fields())
                         .from(MEDIA)
                         .naturalJoin(type)
                         .where(MEDIA.TITLE.contains(search))
+                        .orderBy(order)
+                        .fetchInto(Media.class);
+
+            } else if (type==null){
+                result = create.select(MEDIA.fields())
+                        .from(MEDIA)
+                        .naturalJoin(MEDIAGENRES)
+                        .naturalJoin(GENRES)
+                        .where(MEDIA.TITLE.contains(search).and(GENRES.NAME.eq(genre)))
+                        .orderBy(order)
+                        .fetchInto(Media.class);
+
+            } else if (order==null){
+                result = create.select(MEDIA.fields())
+                        .from(MEDIA)
+                        .naturalJoin(MEDIAGENRES)
+                        .naturalJoin(GENRES)
+                        .naturalJoin(type)
+                        .where(MEDIA.TITLE.contains(search).and(GENRES.NAME.eq(genre)))
+                        .fetchInto(Media.class);
+            } else{
+                result = create.select(MEDIA.fields())
+                        .from(MEDIA)
+                        .naturalJoin(MEDIAGENRES)
+                        .naturalJoin(GENRES)
+                        .naturalJoin(type)
+                        .where(MEDIA.TITLE.contains(search).and(GENRES.NAME.eq(genre)))
                         .orderBy(order)
                         .fetchInto(Media.class);
             }

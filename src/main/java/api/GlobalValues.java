04cbd4bf4eb9b8315d6dc370ae6f2fc76ced1431
Juan Pedro Gálvez <juagallop1@alum.us.es>
2022-05-03 14:00:49 +0200

    Added new database connection data

--- src/main/java/api/GlobalValues.java
+++ src/main/java/api/GlobalValues.java
@@ -2,7 +2,8 @@ package api;
 
 public class GlobalValues {
 
-    public static final String USER = "user";
-    public static final String PASSWORD = "user";
-    public static final String URL = "jdbc:mariadb://localhost:3306/harmony";
+    public static final String USER = "harmony";
+    public static final String PASSWORD = "tN1moow41jd6rGfa";
+    public static final String URL = "jdbc:mysql://34.65.47.107:3306/harmony";
+
 }

ad8beaa6f05231165b5d2c77fa4dd3a7c9142b7c
AntonioRodriguezRuiz <antoniorodriguezruiz2002@gmail.com>
2022-05-03 17:46:03 +0200

    restored page limit

--- src/main/java/api/GlobalValues.java
+++ src/main/java/api/GlobalValues.java
@@ -7,4 +7,5 @@ public class GlobalValues {
     public static final String PASSWORD = "tN1moow41jd6rGfa";
     public static final String URL = "jdbc:mysql://34.65.47.107:3306/harmony";
 
+    public static final Integer PAGE_SIZE = 20;
 }

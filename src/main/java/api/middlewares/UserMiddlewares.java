8ef64e82464b1dae3224baa99d7f6fb1795967f7
Juan Pedro Gálvez <juagallop1@alum.us.es>
2022-05-04 15:31:29 +0200

    Added from Antonio's branch

---
+++ src/main/java/api/middlewares/UserMiddlewares.java
@@ -0,0 +1,40 @@
+package api.middlewares;
+
+import api.GlobalValues;
+import org.jooq.DSLContext;
+import org.jooq.SQLDialect;
+import org.jooq.impl.DSL;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+import src.main.java.model.tables.pojos.Admins;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.List;
+
+import static src.main.java.model.Tables.ADMINS;
+
+public class UserMiddlewares {
+
+    public static void isAdmin(Integer userid) throws SQLException {
+        try (Connection conn = DriverManager.getConnection(GlobalValues.URL, GlobalValues.USER, GlobalValues.PASSWORD)) {
+            DSLContext create = DSL.using(conn, SQLDialect.MYSQL);
+            List<Admins> result = create.select()
+                .from(ADMINS)
+                .where(ADMINS.USERID.eq(userid))
+                .fetchInto(Admins.class);
+
+            if(result.isEmpty()){
+                throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+            }
+
+        } catch (ResponseStatusException | SQLException e){
+            if(e instanceof ResponseStatusException){
+                throw e;
+            }
+            e.printStackTrace();
+        }
+    }
+
+}

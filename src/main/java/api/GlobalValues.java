2d1363dcba51cb9236f4ebb15943e49a919c0920
Juan Pedro Gálvez <juagallop1@alum.us.es>
2022-05-04 23:04:00 +0200

    New connection details

--- src/main/java/api/GlobalValues.java
+++ src/main/java/api/GlobalValues.java
@@ -3,7 +3,7 @@ package api;
 public class GlobalValues {
 
     public static final String USER = "harmony";
-    public static final String PASSWORD = "tN1moow41jd6rGfa";
-    public static final String URL = "jdbc:mysql://34.65.47.107:3306/harmony";
+    public static final String PASSWORD = "k8KAkzsMjJqjFu6w4Va5ni7aaUAqpMa7";
+    public static final String URL = "jdbc:mariadb://db.harmonyapi.rocks:3306/harmony";
 
 }

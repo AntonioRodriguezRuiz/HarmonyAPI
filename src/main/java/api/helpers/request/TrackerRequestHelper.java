b18c0cc3ebedbbf76c014360e9bf17402b527e5d
Juan Pedro Gálvez <juagallop1@alum.us.es>
2022-05-06 09:43:30 +0200

    Added TrackerRequestHelper

---
+++ src/main/java/api/helpers/request/TrackerRequestHelper.java
@@ -0,0 +1,11 @@
+package api.helpers.request;
+
+/**
+ * TrackerRequestHelper
+ * Project HarmonyAPI
+ * Created: 2022-05-06
+ *
+ * @author juagallop1
+ **/
+public record TrackerRequestHelper(Integer mediaId, Integer state) {
+}

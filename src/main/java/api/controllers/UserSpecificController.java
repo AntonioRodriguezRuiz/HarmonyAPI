5a3f89de690f88a20c96e3cd53e49bb09910ef9c
Juan Pedro Gálvez <juagallop1@alum.us.es>
2022-05-06 09:44:38 +0200

    Added postTracker

--- src/main/java/api/controllers/UserSpecificController.java
+++ src/main/java/api/controllers/UserSpecificController.java
@@ -1,10 +1,17 @@
 package api.controllers;
 
+import api.helpers.enums.TrackerState;
+import api.helpers.request.TrackerRequestHelper;
 import api.helpers.response.TrackerResponseHelper;
 import api.helpers.response.UserResponseHelper;
+import api.services.MediaService;
+import api.services.UserService;
 import api.services.UserSpecificService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
 import java.sql.SQLException;
 import java.util.List;
 
@@ -21,15 +28,33 @@ import java.util.List;
 public class UserSpecificController {
 
     @Autowired
-    UserSpecificService userService;
+    private MediaService mediaService;
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private UserSpecificService userSpecificService;
 
     @GetMapping
     public UserResponseHelper getUser(@PathVariable Integer id) throws SQLException {
-        return userService.getUser(id);
+        return userSpecificService.getUser(id);
     }
 
     @GetMapping("/tracking")
     public List<TrackerResponseHelper> getTracking(@PathVariable Integer id) throws SQLException {
-        return userService.getTracking(id);
+        return userSpecificService.getTracking(id);
+    }
+
+    @PostMapping("/tracking")
+    public TrackerResponseHelper postTracker(@PathVariable Integer id, @RequestBody TrackerRequestHelper tracker) throws SQLException {
+        if (TrackerState.of(tracker.state()) == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+        if (!mediaService.mediaExists(tracker.mediaId())) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        if (!userService.userExists(id)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        return userSpecificService.postTracker(id, tracker);
     }
 }

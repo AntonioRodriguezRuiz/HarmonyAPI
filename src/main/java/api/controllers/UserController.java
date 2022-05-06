4333b6586bf56d1b17b1f0609b103efeb5561c8a
Juan Pedro Gálvez <juagallop1@alum.us.es>
2022-05-06 09:56:47 +0200

    Added docs annotations

--- src/main/java/api/controllers/UserController.java
+++ src/main/java/api/controllers/UserController.java
@@ -3,11 +3,15 @@ package api.controllers;
 import api.helpers.request.UserRequestHelper;
 import api.helpers.response.UserResponseHelper;
 import api.services.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 
 import java.sql.SQLException;
 
@@ -26,6 +30,12 @@ public class UserController {
     @Autowired
     UserService userService;
 
+    @Operation(summary = "Create a new user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "User created", content = @Content),
+            @ApiResponse(responseCode = "400", description = "Bad request", content = @Content),
+            @ApiResponse(responseCode = "409", description = "User already exists", content = @Content)
+    })
     @PostMapping
     public ResponseEntity<UserResponseHelper> postUser(@RequestBody UserRequestHelper user) throws SQLException {
         if (user.username() == null || user.password() == null || user.email() == null) {

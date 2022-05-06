3f7e3c1506b32c5965162e0827dfa2246d629029
Vicky Moreno <victoriaemc10@gmail.com>
2022-05-06 17:11:26 +0200

    Get all reports Delete a report (admin only)

--- src/main/java/api/controllers/ReportController.java
+++ src/main/java/api/controllers/ReportController.java
@@ -1,6 +1,9 @@
 package api.controllers;
 
+import api.GlobalValues;
+import api.helpers.request.ReportRequestHelper;
 import api.helpers.request.UseridBodyHelper;
+import api.helpers.response.ReportResponseHelper;
 import api.middlewares.UserMiddlewares;
 import api.services.ReportService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -11,11 +14,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import src.main.java.model.tables.pojos.Reports;
 
 import java.sql.SQLException;
+import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/reports/{id}")
+@RequestMapping("/api/v1/reports")
 public class ReportController {
     @Autowired
     ReportService reportService;
@@ -27,10 +33,31 @@ public class ReportController {
             @ApiResponse(responseCode = "403", description = "Not enough permissions", content = @Content),
             @ApiResponse(responseCode = "404", description = "Item doesn't exists", content = @Content)
     })
-    @DeleteMapping
+    @DeleteMapping("/{id}")
     public ResponseEntity deleteReport(@PathVariable Integer id, @RequestBody UseridBodyHelper useridBody) throws SQLException {
         UserMiddlewares.isAdmin(useridBody.userid());
         reportService.deleteReport(id);
         return new ResponseEntity(HttpStatus.FORBIDDEN);
     }
+
+
+    @Operation(summary = "Get all reports")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Search successful")})
+    @GetMapping
+    public List<Reports> getAllReports(@RequestParam(name="page", required = false) String pageParam, @RequestBody UseridBodyHelper user) throws SQLException{
+        UserMiddlewares.isAdmin(user.userid());
+        Integer page = pageParam!=null ? Integer.valueOf(pageParam) : 1;
+        Integer offset = page * GlobalValues.PAGE_SIZE - GlobalValues.PAGE_SIZE;
+        return ReportService.getAllReports(offset);
+    }
+
+    @PostMapping
+    public ResponseEntity<ReportResponseHelper> postReport(@RequestBody ReportRequestHelper report) throws SQLException {
+        if(report.useridreported() == null || report.reviewid() == null){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>(reportService.postReport(report), HttpStatus.CREATED);
+    }
+
 }

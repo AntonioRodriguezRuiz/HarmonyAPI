77a136167d06fb27d67dc92372ea334ee269ec5b
AntonioRodriguezRuiz <antoniorodriguezruiz2002@gmail.com>
2022-05-06 15:06:37 +0200

    Added getAllPlatforms controller function

--- src/main/java/api/controllers/PlatformsController.java
+++ src/main/java/api/controllers/PlatformsController.java
@@ -1,9 +1,16 @@
 package api.controllers;
 
 import api.services.PlatformsService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import src.main.java.model.tables.Platforms;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/platforms")
@@ -11,5 +18,13 @@ public class PlatformsController {
 
     @Autowired
     PlatformsService platformsService;
+
+    @Operation(summary = "Get all the platforms for videogames")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Fetch successful")})
+    @GetMapping
+    public List<Platforms> getAllPlatforms(){
+        return platformsService.getAllPlatforms();
+    }
 
 }

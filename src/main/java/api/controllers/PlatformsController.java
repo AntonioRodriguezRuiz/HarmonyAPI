801cb7a3dee759fa64e73601254a5a8490e89dda
AntonioRodriguezRuiz <antoniorodriguezruiz2002@gmail.com>
2022-05-06 15:25:19 +0200

    Fixed imports

--- src/main/java/api/controllers/PlatformsController.java
+++ src/main/java/api/controllers/PlatformsController.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import src.main.java.model.tables.Platforms;
+import src.main.java.model.tables.pojos.Platforms;
 
 import java.util.List;
 
--- src/main/java/api/services/PlatformsService.java
+++ src/main/java/api/services/PlatformsService.java
@@ -5,23 +5,25 @@ import org.jooq.DSLContext;
 import org.jooq.SQLDialect;
 import org.jooq.impl.DSL;
 import org.springframework.stereotype.Service;
-import src.main.java.model.tables.Platforms;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.util.List;
 
 import static src.main.java.model.Tables.PLATFORMS;
+import src.main.java.model.tables.pojos.Platforms;
+
 
 @Service
 public class PlatformsService {
+
     public List<Platforms> getAllPlatforms() {
         List<Platforms> platformsList = null;
 
         try (Connection conn = DriverManager.getConnection(GlobalValues.URL, GlobalValues.USER, GlobalValues.PASSWORD)) {
             DSLContext create = DSL.using(conn, SQLDialect.MARIADB);
 
-            platformsList = create.select()
+            platformsList = create.select(PLATFORMS.fields())
                     .from(PLATFORMS)
                     .fetchInto(Platforms.class);
 

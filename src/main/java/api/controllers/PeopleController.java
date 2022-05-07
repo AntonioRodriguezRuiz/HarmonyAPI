3490da6a4ec4711c8cc28b6877ed2f6365f6d564
AntonioRodriguezRuiz <antoniorodriguezruiz2002@gmail.com>
2022-05-07 18:04:53 +0200

    Removed unused imports

--- src/main/java/api/controllers/PeopleController.java
+++ src/main/java/api/controllers/PeopleController.java
@@ -1,41 +1,23 @@
 package api.controllers;
 
-import api.GlobalValues;
-import api.helpers.request.BookRequestHelper;
-import api.helpers.request.GenreRequestHelper;
 import api.helpers.request.PeopleRequestHelper;
-import api.helpers.response.MediaResponseHelper;
 import api.middlewares.PeopleMiddlewares;
 import api.middlewares.UserMiddlewares;
 import api.services.PeopleService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import org.jooq.DSLContext;
-import org.jooq.SQLDialect;
-import org.jooq.SortField;
-import org.jooq.TableLike;
-import org.jooq.impl.DSL;
-import org.jooq.Record;
-import org.jooq.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import org.springframework.web.server.ResponseStatusException;
-import src.main.java.model.tables.pojos.Media;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.List;
-import java.util.Map;
 
-import static src.main.java.model.tables.People.PEOPLE;
-import static src.main.java.model.tables.Media.MEDIA;
 import src.main.java.model.tables.pojos.People;
 
 @RestController
@@ -49,7 +31,7 @@ public class PeopleController {
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Search successful")})
     @GetMapping
-    public List<src.main.java.model.tables.pojos.People> getAllPeople() throws SQLException {
+    public List<People> getAllPeople() throws SQLException {
         return peopleService.getAllPeople();
 
     }

7b1e1261d812c403ea336945e885e37a8e7cd4ff
Juan Pedro Gálvez <juagallop1@alum.us.es>
2022-05-10 11:33:22 +0200

    refactor: using .env variables for database connection details

--- src/main/java/api/GlobalValues.java
+++ src/main/java/api/GlobalValues.java
@@ -1,10 +1,12 @@
 package api;
 
+import static com.harmonyapi.Application.dotenv;
+
 public class GlobalValues {
 
-    public static final String USER = "harmony";
-    public static final String PASSWORD = "k8KAkzsMjJqjFu6w4Va5ni7aaUAqpMa7";
-    public static final String URL = "jdbc:mariadb://db.harmonyapi.rocks:3306/harmony";
+    public static final String USER = dotenv.get("HARMONY_DB_USER");
+    public static final String PASSWORD = dotenv.get("HARMONY_DB_PASSWORD");
+    public static final String URL = dotenv.get("HARMONY_DB_URL");
 
     public static final Integer PAGE_SIZE = 20;
 }
